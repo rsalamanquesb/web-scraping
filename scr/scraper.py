2ea922be25e081b225f82b4ae6ff6e46edaafa00
rsalamanquesb <37367406+rsalamanquesb@users.noreply.github.com>
2018-03-23 08:53:35 +0100

    Rsalamanquesb p1 (#13)
    * Update README.md
    
    * New commit: modification of main.py file
    
    * New commit: modification of main.py file
    
    * This is the last version
    
    * This is the last version
    
    * Creación del fichero main.py
    
    * Creación del fichero scraper.py
    
    * Creación del fichero prueba.csv
    
    * This is the last version
    
    * This is the last version
    
    * Sincronización con el master (#8)
    
    * Rpcdesmo patch 1 (#5)
    
    * Update README.md
    
    * New commit: modification of main.py file
    
    * New commit: modification of main.py file
    
    * Rpcdesmo patch 1 (#6)
    
    * Update README.md
    
    * New commit: modification of main.py file
    
    * New commit: modification of main.py file
    
    * This is the last version
    
    * This is the last version
    
    * Rpcdesmo patch 1 (#7)
    
    * Update README.md
    
    * New commit: modification of main.py file
    
    * New commit: modification of main.py file
    
    * This is the last version
    
    * This is the last version
    
    * Creación del fichero main.py
    
    * Creación del fichero scraper.py
    
    * This is the last version
    
    * This is the last version
    
    * Prueba de concepto de Scrapper con una URL fija.
    
    * Merge from master (#11)
    
    * Rpcdesmo patch 1 (#7)
    
    * Update README.md
    
    * New commit: modification of main.py file
    
    * New commit: modification of main.py file
    
    * This is the last version
    
    * This is the last version
    
    * Creación del fichero main.py
    
    * Creación del fichero scraper.py
    
    * This is the last version
    
    * This is the last version
    
    * Rsalamanquesb p1 (#10)
    
    * Creación del fichero prueba.csv
    
    * Sincronización con el master (#8)
    
    * Rpcdesmo patch 1 (#5)
    
    * Update README.md
    
    * New commit: modification of main.py file
    
    * New commit: modification of main.py file
    
    * Rpcdesmo patch 1 (#6)
    
    * Update README.md
    
    * New commit: modification of main.py file
    
    * New commit: modification of main.py file
    
    * This is the last version
    
    * This is the last version
    
    * Rpcdesmo patch 1 (#7)
    
    * Update README.md
    
    * New commit: modification of main.py file
    
    * New commit: modification of main.py file
    
    * This is the last version
    
    * This is the last version
    
    * Creación del fichero main.py
    
    * Creación del fichero scraper.py
    
    * This is the last version
    
    * This is the last version
    
    * Prueba de concepto de Scrapper con una URL fija.
    
    * Mejora al buscar la tabla objetivo.
    
    * Main funcional.
    scraper.py pasa a ser una clase.
    
    * Main funcional.
    scraper.py pasa a ser una clase.
    
    * Main funcional.
    scraper.py pasa a ser una clase.

--- scr/scraper.py
+++ scr/scraper.py
@@ -1,58 +1,71 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-url = 'http://eportal.mapama.gob.es/BoleHWeb/accion/cargador_pantalla.htm?screen_code=60030&screen_language=&' \
-	'bh_number=12&bh_year=2018&bh_amb_name=Cant%E1brico%20Oriental&bh_amb_id=17&bh_date='
-url2 = 'http://eportal.mapama.gob.es/BoleHWeb/accion/cargador_pantalla.htm?screen_code=60030&screen_language=&' \
-	'bh_number=12&bh_year=2018&bh_amb_name=Cant%E1brico%20Occidental&bh_amb_id=12&bh_date='
-url3 = 'http://eportal.mapama.gob.es/BoleHWeb/accion/cargador_pantalla.htm?screen_code=60030&screen_language=&' \
-	'bh_number=12&bh_year=2018&bh_amb_name=Tajo&bh_amb_id=3&bh_date='
+class ReservasHidraulicas:
+
+    def __init__(self):
+        self.url = 'http://eportal.mapama.gob.es/BoleHWeb/accion/cargador_pantalla.htm?screen_code=60030&screen_language=&' \
+            'bh_number=12&bh_year=2018&bh_amb_name=Cant%E1brico%20Oriental&bh_amb_id=17&bh_date='
+        self.url2 = 'http://eportal.mapama.gob.es/BoleHWeb/accion/cargador_pantalla.htm?screen_code=60030&screen_language=&' \
+            'bh_number=12&bh_year=2018&bh_amb_name=Cant%E1brico%20Occidental&bh_amb_id=12&bh_date='
+        self.url3 = 'http://eportal.mapama.gob.es/BoleHWeb/accion/cargador_pantalla.htm?screen_code=60030&screen_language=&' \
+            'bh_number=12&bh_year=2018&bh_amb_name=Tajo&bh_amb_id=3&bh_date='
+
+    def tratarTexto(self, texto):
+
+        texto = texto.replace(u'\xa0', '')
+        texto = texto.replace('\n', ' ')
+        texto = texto.replace('\t', ' ')
+        texto = texto.strip()
+
+        return texto
+
+    def tratarURL(self, url):
+
+        html = urlopen(self.url).read()
+
+        cabecera1 = []
+        cabecera2 = []
+        datos = []
+
+        soup = BeautifulSoup(html, 'html.parser')
+
+        table = soup.find('table',
+                          {"width": "90%", "cellspacing": "1", "cellpadding": "1", "border": 0, "align": "center"})
+
+        tr = table.find_all('tr')
+
+        for i in range(0, len(tr)):
+
+            # Inicialización para el bucle tr
+            td = tr[i].find_all('td')
+            texto = ''
+            linea = []
+
+            for n in range(0, len(td)):
+                if i == 0:
+                    # Rellenar la cabecera1
+                    cabecera1.append(self.tratarTexto(td[n].text))
+                elif i == 1:
+                    # Rellenar la cabecera2
+                    cabecera2.append(self.tratarTexto(td[n].text))
+                else:
+                    # Rellenar datos de la fila
+                    linea.append(self.tratarTexto(td[n].text))
+
+            if i > 1:
+                # Añadir fila a la matriz de filas
+                datos = datos + [linea]
+
+        # Obtener la zona hidrográfica
+        zona = self.tratarTexto(soup.find('td', {"class": "tdsubtitulo"}).text)
+
+        print(zona)
+        print(cabecera1)
+        print(cabecera2)
+        print(datos)
 
 
-html = urlopen(url3).read()
+    def scrape(self):
 
-cabecera1 = []
-cabecera2 = []
-datos = []
-
-soup = BeautifulSoup(html, 'html.parser')
-
-table = soup.find_all('table')
-
-tr = table[14].find_all('tr')
-
-for i in range (0, len(tr)):
-
-	#Inicialización para el bucle tr
-	td = tr[i].find_all('td')
-	texto = ''
-	linea = []
-
-	for n in range (0, len(td)):
-		if i == 0:
-			#Rellenar la cabecera1
-			texto = td[n].text.replace(u'\xa0','')
-			texto = texto.replace('\n', '')
-			texto = texto.replace('\t', '')
-			texto = texto.replace(' ', '')
-			cabecera1.append(texto)
-		elif i == 1:
-			# Rellenar la cabecera2
-			texto = td[n].text.replace(u'\xa0','')
-			texto = texto.replace('\n', '')
-			texto = texto.replace('\t', '')
-			texto = texto.replace(' ', '')
-			cabecera2.append(texto)
-		else:
-			texto = td[n].text.replace(u'\xa0','')
-			texto = texto.replace('\n', '')
-			texto = texto.replace('\t', '')
-			texto = texto.replace(' ', '')
-			linea.append(texto)
-
-	if i > 1:
-		datos = datos + [linea]
-
-print(cabecera1)
-print(cabecera2)
-print(datos)
+        self.tratarURL(self.url2)
