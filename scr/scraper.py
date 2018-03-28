79c38b50c8ae09937ae1d3fb72105dc9d2517348
rpcdesmo <37365710+rpcdesmo@users.noreply.github.com>
2018-03-28 11:50:58 +0200

    Versión 1.0

--- scr/scraper.py
+++ scr/scraper.py
@@ -12,6 +12,10 @@ class ReservasHidraulicas:
             'bh_number=12&bh_year=2018&bh_amb_name=Cant%E1brico%20Oriental&bh_amb_id=17&bh_date='
         self.url2 = 'http://eportal.mapama.gob.es/BoleHWeb/accion/cargador_pantalla.htm?screen_code=60030&screen_language=&' \
             'bh_number=12&bh_year=2018&bh_amb_name=Cant%E1brico%20Occidental&bh_amb_id=12&bh_date='
+
+        # self.url2 = 'http://eportal.mapama.gob.es/BoleHWeb/accion/cargador_pantalla.htm?screen_code=60030&screen_language=&' \
+        #     'bh_number=10&bh_year=2018&bh_amb_name=%27+escape(%27Cuencas%20Internas%20de%20Catalu%C3%B1a%27)+%27&bh_amb_id=10&bh_date=03/03/2018'
+
         self.url3 = 'http://eportal.mapama.gob.es/BoleHWeb/accion/cargador_pantalla.htm?screen_code=60030&screen_language=&' \
             'bh_number=12&bh_year=2018&bh_amb_name=Tajo&bh_amb_id=3&bh_date='
 
@@ -26,12 +30,13 @@ class ReservasHidraulicas:
         self.coleccionURLconDatos = []
 
         #Las fechas las definimos a mano. En una versión posterior, será el usuario el que introduzca el periodo
-        self.fechaIni = '2018-03-02'
-        self.fechaFin = '2018-03-05'
+        self.fechaIni = '2018-03-03'
+        self.fechaFin = '2018-03-04'
 
     def cargarColeccionURLdePartida(self):
 
         ##El primer paso será cargar un array con un intervalo de fechas a partir de fecha inicio y fecha fin
+        print("\n##############Comienza la carga de URL principales##############\n")
 
         fechas = [] #array de fechas con el formato necesario para la url
         fechasAux = np.arange(self.fechaIni, self.fechaFin, dtype='datetime64[D]') #generación de fechas como datetime64[D]
@@ -49,30 +54,51 @@ class ReservasHidraulicas:
             print("Agregada URL para búsqueda: " + auxUrlBase)
             self.coleccionURLdePartida.append(auxUrlBase)
 
+        print("\n##############Finalizada la carga de URL principales##############\n")
+
     def cargarURLconDatos(self):
 
         print("\n##############Comienza la carga de URL con datos##############\n")
+
+        #Definimos esta url para concatenarle las direcciones que encontremos en cada página principal
+        base_url = 'http://eportal.mapama.gob.es/BoleHWeb/accion/cargador_pantalla.htm?'
+
         for url in self.coleccionURLdePartida:
+
 
             html = urlopen(url).read()
             soup = BeautifulSoup(html, 'html.parser')
 
             table = soup.find('table',
-                              {"width": "90%", "cellspacing": "1", "cellpadding": "1", "border": 0, "align": "center"})
+                              {"align": "center", "border": 0, "cellpadding": "1", "cellspacing": "1"})
 
             tr = table.find_all('tr')
 
             for i in range(0, len(tr)):
 
                 # Inicialización para el bucle tr
-                td = tr[i].find_all('td')
+                cols = tr[i].find_all('td')
                 linea = []
 
-                for n in range(0, len(td)):
-                    print(n)
+                for n in range(0, len(cols)):
+                    # print(n)
+                    if n == 2:
+                        # Aquí tenemos parte de la url que tendremos que usar
+                        td_aux = cols[n]
+                        link = td_aux.find('a').get('href')
 
-            #         if i == 0:
-            #             # Rellenar la cabecera1
+                        #necesitamos quedarnos con la parte desde 'screen_code=60030' inclusive en adelante
+                        #no encuentro la forma elegante de hacerlo, de momento queda así...
+                        print(link)
+                        link = link[-116:]
+                        link = link.replace('+escape(,','')
+                        link = link.replace(')', '')
+
+                        print(link)
+                        self.coleccionURLconDatos.append(link)
+
+                        #base_url = 'http://eportal.mapama.gob.es/BoleHWeb/accion/cargador_pantalla.htm?'
+
             #             cabecera1.append(self.tratarTexto(td[n].text))
             #         elif i == 1:
             #             # Rellenar la cabecera2
